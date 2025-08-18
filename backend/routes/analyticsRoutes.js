2df6547008bf3a26ddc5ff65ad08576a9488aacf
aditi-2004 <aditi2914.goel@gmail.com>
2025-08-18 10:20:00 +0530

    Updated Analytics.js Routes

--- backend/routes/analyticsRoutes.js
+++ backend/routes/analyticsRoutes.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/authMiddleware');
 const { getUserAnalytics, getProgressTrends } = require('../controllers/analyticsController');
-
 router.get('/user', auth, getUserAnalytics);
 router.get('/trends', auth, getProgressTrends);
-
 module.exports = router;
